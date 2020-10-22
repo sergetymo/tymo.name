4d078734e8f78fef01f9ffe77b793bfc114b56cf
Serge Tymoshenko <serge@tymo.name>
2020-10-22 16:21:41 +0300

    Fix linting issue, pretty print uploading result

--- deploy.js
+++ deploy.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 const args = process.argv.slice(2)
 
 if (args.length === 0) {
   const dotenv = require('dotenv')
   dotenv.config()
 }
-
-console.log(process.env)
 
 const FtpDeploy = require('ftp-deploy')
 const path = require('path')
@@ -27,7 +27,7 @@ if (process.env.FTP_USER && process.env.FTP_PASS && process.env.FTP_HOST) {
 
   ftp
     .deploy(config)
-    .then(result => console.log(`Finished: ${result}`))
+    .then(result => console.log(`Finished:\n${result[0].join('\n')}`))
     .catch(error => console.log(error))
 } else {
   console.error('Couldn\'t find needful env vars')
